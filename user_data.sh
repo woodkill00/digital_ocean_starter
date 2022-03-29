c3beecf89b55f221f23012f43c457d871ee21f5c
WoodKill <43331523+woodkill00@users.noreply.github.com>
2022-03-29 01:13:32 -0600

    Create user_data.sh

---
+++ user_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get upgrade -y
+apt-get autoremove -y
+adduser --uid 1000 --disabled-password --gecos GECOS {USER}
+usermod -aG sudo {USER}
+mkdir /home/{USER}/.ssh
+touch /home/{USER}/.ssh/authorized_keys
+cp .ssh/authorized_keys /home/{USER}/.ssh/authorized_keys
+chmod 700 /home/{USER}/.ssh
+chmod 600 /home/{USER}/.ssh/authorized_keys
+chown 1000:1000 -R /home/{USER}/.ssh
