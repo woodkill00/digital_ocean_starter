043fce7c6a8a9a7b287a92d1819bd4e41e3c1687
WoodKill <43331523+woodkill00@users.noreply.github.com>
2022-03-29 01:51:08 -0600

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -7,12 +7,15 @@ apt-get autoremove -y
 adduser --uid 1000 --disabled-password --gecos GECOS {USER}
 usermod -aG sudo {USER}
 mkdir /home/{USER}/.ssh
-mkdir /home/{USER}/scripts
 touch /home/{USER}/.ssh/authorized_keys
+cd ~/
 cp .ssh/authorized_keys /home/{USER}/.ssh/authorized_keys
 chmod 700 /home/{USER}/.ssh
 chmod 600 /home/{USER}/.ssh/authorized_keys
 chown 1000:1000 -R /home/{USER}/.ssh
-
-curl -SL https://github.com/woodkill00/digital_ocean_starter.git -o /home/{USER}/scripts
+sudo apt install git-all -y
+mkdir /home/{USER}/scripts
+cd /home/{USER}/scripts
+git clone https://github.com/woodkill00/digital_ocean_starter.git .
 chown 1000:1000 -R /home/{USER}/scripts
+reboot now
